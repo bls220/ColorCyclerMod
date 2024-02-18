9d2623e1a0072b967056569c63ff43ce59aa2d7a
Elmotrix <kim.hoftvedt@gmail.com>
2024-02-18 21:28:47 +0100

    Add project files.

---
+++ ColorCycler/BepInEx.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ColorCycler
+{
+    #region BepInEx
+    [BepInEx.BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class ColorCyclerBep : BepInEx.BaseUnityPlugin
+    {
+        public const string pluginGuid = "net.elmo.stationeers.ColorCycler";
+        public const string pluginName = "ColorCycler";
+        public const string pluginVersion = "1.0";
+        public static void Log(string line)
+        {
+            Debug.Log("[" + pluginName + "]: " + line);
+        }
+        void Awake()
+        {
+            try
+            {
+                var harmony = new Harmony(pluginGuid);
+                harmony.PatchAll();
+                Log("Patch succeeded");
+            }
+            catch (Exception e)
+            {
+                Log("Patch Failed");
+                Log(e.ToString());
+            }
+        }
+    }
+    #endregion
+}
---
+++ ColorCycler/ColorCyclerMod.cs
@@ -0,0 +1,79 @@
+﻿using Assets.Scripts;
+using Assets.Scripts.Inventory;
+using Assets.Scripts.Objects;
+using Assets.Scripts.Objects.Items;
+using HarmonyLib;
+using JetBrains.Annotations;
+using System.Collections;
+
+namespace ColorCycler
+{
+    [HarmonyPatch(typeof(InventoryManager), "NormalMode")]
+    public class ColorCyclerMod
+    {
+        [UsedImplicitly]
+        public static void Prefix(InventoryManager __instance)
+        {
+            bool keyUp = __instance.newScrollData > 0f;
+            bool keyDown = __instance.newScrollData < 0f;
+            if ((bool)__instance.ActiveHand.Slot.Get() && (keyUp || keyDown))
+            {
+                if (__instance.ActiveHand.Slot.Get() is SprayCan sprayCan)
+                {
+                    int current = -1;
+                    for (int i = 0; i < GameManager.Instance.CustomColors.Count; i++)
+                    {
+                        if (GameManager.Instance.CustomColors[i].Normal == sprayCan.PaintMaterial)
+                        {
+                            current = i;
+                            break;
+                        }
+                    }
+                    if (keyUp)
+                    {
+                        current++;
+                    }
+                    else if (keyDown)
+                    {
+                        current--;
+                    }
+                    if (current >= GameManager.Instance.CustomColors.Count)
+                    {
+                        current = 0;
+                    }
+                    if (current < 0)
+                    {
+                        current = GameManager.Instance.CustomColors.Count-1;
+                    }
+                    sprayCan.PaintableMaterial = GameManager.Instance.CustomColors[current].Normal;
+                    sprayCan.PaintMaterial = GameManager.Instance.CustomColors[current].Normal;
+                    foreach (Thing thing in Prefab.AllPrefabs)
+                    {
+                        if (thing is SprayCan sprayCan2)
+                        {
+                            if (sprayCan2.PaintMaterial == sprayCan.PaintMaterial)
+                            {
+                                sprayCan.SetPrefab(sprayCan2);
+                                sprayCan.RenameThing(sprayCan2.DisplayName);
+                                sprayCan.Thumbnail = sprayCan2.Thumbnail;
+                                __instance.ActiveHand.Slot.RefreshSlotDisplay();
+                            }
+                        }
+                    }
+                    sprayCan.Quantity = sprayCan.MaxQuantity;
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(SprayCan), "OnUseItem")]
+    public class SprayCanOnUseItemPAtch
+    {
+        [UsedImplicitly]
+        public static bool Prefix(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
